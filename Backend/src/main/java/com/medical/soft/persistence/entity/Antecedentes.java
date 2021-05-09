a32b69b58f6251c05585705f460f08aa65fcdb55
Yuberley <yuberleyguerrero1999@gmail.com>
2021-05-09 01:17:19 -0500

    Modeling architecture for BD part 3

---
+++ Backend/src/main/java/com/medical/soft/persistence/entity/Antecedentes.java
@@ -0,0 +1,124 @@
+package com.medical.soft.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "antecedentes")
+public class Antecedentes {
+
+    @Id
+    @Column(name ="cod_antecedente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codAntecedente;
+
+    // Attributes
+    private String psicososiales;
+    private String familiares;
+    private String quirurgicos;
+    private String hospitalarios;
+    private String patologicos;
+    private String farmacologicos;
+    @Column(name = "toxico_alergicos")
+    private String toxicoAlergicos;
+    private String traumaticos;
+    @Column(name = "gineco_obstetrico")
+    private String ginecoObstetrico;
+    private String inmunologicos;
+    private String psiquiatricos;
+
+    public Integer getCodAntecedente() {
+        return codAntecedente;
+    }
+
+    public void setCodAntecedente(Integer codAntecedente) {
+        this.codAntecedente = codAntecedente;
+    }
+
+    public String getPsicososiales() {
+        return psicososiales;
+    }
+
+    public void setPsicososiales(String psicososiales) {
+        this.psicososiales = psicososiales;
+    }
+
+    public String getFamiliares() {
+        return familiares;
+    }
+
+    public void setFamiliares(String familiares) {
+        this.familiares = familiares;
+    }
+
+    public String getQuirurgicos() {
+        return quirurgicos;
+    }
+
+    public void setQuirurgicos(String quirurgicos) {
+        this.quirurgicos = quirurgicos;
+    }
+
+    public String getHospitalarios() {
+        return hospitalarios;
+    }
+
+    public void setHospitalarios(String hospitalarios) {
+        this.hospitalarios = hospitalarios;
+    }
+
+    public String getPatologicos() {
+        return patologicos;
+    }
+
+    public void setPatologicos(String patologicos) {
+        this.patologicos = patologicos;
+    }
+
+    public String getFarmacologicos() {
+        return farmacologicos;
+    }
+
+    public void setFarmacologicos(String farmacologicos) {
+        this.farmacologicos = farmacologicos;
+    }
+
+    public String getToxicoAlergicos() {
+        return toxicoAlergicos;
+    }
+
+    public void setToxicoAlergicos(String toxicoAlergicos) {
+        this.toxicoAlergicos = toxicoAlergicos;
+    }
+
+    public String getTraumaticos() {
+        return traumaticos;
+    }
+
+    public void setTraumaticos(String traumaticos) {
+        this.traumaticos = traumaticos;
+    }
+
+    public String getGinecoObstetrico() {
+        return ginecoObstetrico;
+    }
+
+    public void setGinecoObstetrico(String ginecoObstetrico) {
+        this.ginecoObstetrico = ginecoObstetrico;
+    }
+
+    public String getInmunologicos() {
+        return inmunologicos;
+    }
+
+    public void setInmunologicos(String inmunologicos) {
+        this.inmunologicos = inmunologicos;
+    }
+
+    public String getPsiquiatricos() {
+        return psiquiatricos;
+    }
+
+    public void setPsiquiatricos(String psiquiatricos) {
+        this.psiquiatricos = psiquiatricos;
+    }
+}
---
+++ Backend/src/main/java/com/medical/soft/persistence/entity/ExamenNeurologico.java
@@ -0,0 +1,89 @@
+package com.medical.soft.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "examen_neurologico")
+public class ExamenNeurologico {
+
+    @Id
+    @Column(name = "cod_en")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codEN;
+
+    // Attributes
+    private String mental;
+    @Column(name = "pares_craneales")
+    private String paresCraneales;
+    @Column(name = "motor_raquideo")
+    private String motorRaquideo;
+    @Column(name = "sensitivo_raquideo")
+    private String sensitivoRaquideo;
+    private String reflejos;
+    private String coordinacion;
+    private String marcha;
+
+    public Integer getCodEN() {
+        return codEN;
+    }
+
+    public void setCodEN(Integer codEN) {
+        this.codEN = codEN;
+    }
+
+    public String getMental() {
+        return mental;
+    }
+
+    public void setMental(String mental) {
+        this.mental = mental;
+    }
+
+    public String getParesCraneales() {
+        return paresCraneales;
+    }
+
+    public void setParesCraneales(String paresCraneales) {
+        this.paresCraneales = paresCraneales;
+    }
+
+    public String getMotorRaquideo() {
+        return motorRaquideo;
+    }
+
+    public void setMotorRaquideo(String motorRaquideo) {
+        this.motorRaquideo = motorRaquideo;
+    }
+
+    public String getSensitivoRaquideo() {
+        return sensitivoRaquideo;
+    }
+
+    public void setSensitivoRaquideo(String sensitivoRaquideo) {
+        this.sensitivoRaquideo = sensitivoRaquideo;
+    }
+
+    public String getReflejos() {
+        return reflejos;
+    }
+
+    public void setReflejos(String reflejos) {
+        this.reflejos = reflejos;
+    }
+
+    public String getCoordinacion() {
+        return coordinacion;
+    }
+
+    public void setCoordinacion(String coordinacion) {
+        this.coordinacion = coordinacion;
+    }
+
+    public String getMarcha() {
+        return marcha;
+    }
+
+    public void setMarcha(String marcha) {
+        this.marcha = marcha;
+    }
+}
