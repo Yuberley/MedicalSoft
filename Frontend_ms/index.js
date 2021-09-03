ecda3d2ac2f4a27c67ccc9a205655d560b3ab6c9
Yuberley <yuberleyguerrero1999@gmail.com>
2021-09-03 14:27:28 -0500

    Routes changes

--- Frontend_ms/index.js
+++ Frontend_ms/index.js
@@ -9,6 +9,14 @@ fetch(`${API}/people`)
                                                 <td>${person.firstName}</td>
                                                 <td>${person.lastName}</td>
                                                 <td>${person.sex}</td>
-                                                <td><a href="http://localhost:8060/medicalsoft/api/clinic_history/${person.personId}"><button type="button" class="btn btn-outline-info">Ver</button></a></td></tr>`);
+                                                <td><a href="/medicalsoft/historia_clinica/?id=${person.personId}"><button type="button" class="btn btn-outline-info">Ver</button></a></td></tr>`);
     HTMLResponse.innerHTML = template;
   });
+
+
+
+var prueba = "Texto de prueba";
+
+function Hola(){
+  return "prueba";
+}
---
+++ Frontend_ms/js/historia_clinica.js
@@ -0,0 +1,3 @@
+var params = new URLSearchParams(window.location.search);
+
+console.log(params)
