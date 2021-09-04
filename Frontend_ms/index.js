41c750375eab819c305136cc0b3f851f0414f2ec
Yuberley <yuberleyguerrero1999@gmail.com>
2021-09-04 00:33:35 -0500

    add js

--- Frontend_ms/index.js
+++ Frontend_ms/index.js
@@ -2,21 +2,24 @@ const API = "http://localhost:8060/medicalsoft/api";
 
 const HTMLResponse = document.querySelector("#personas");
 
-fetch(`${API}/people`)
-  .then(response => response.json())
-  .then(people => {
-    const template = people.map((person) => `<tr><th>${person.personId}</th>
-                                                <td>${person.firstName}</td>
-                                                <td>${person.lastName}</td>
-                                                <td>${person.sex}</td>
-                                                <td><a href="/medicalsoft/historia_clinica/?id=${person.personId}"><button type="button" class="btn btn-outline-info">Ver</button></a></td></tr>`);
-    HTMLResponse.innerHTML = template;
-  });
-
-
-
-var prueba = "Texto de prueba";
-
-function Hola(){
-  return "prueba";
+async function getPeople(){
+  const result = await fetch(`${API}/people`)
+                .then(response => response.json())
+                .then(people => {
+                  var template = people.map((person) => `<tr><th>${person.personId}</th>
+                                                              <td>${person.firstName}</td>
+                                                              <td>${person.lastName}</td>
+                                                              <td>${person.sex}</td>
+                                                              <td><a href="/medicalsoft/historia_clinica/?id=${person.personId}">
+                                                                    <button type="button" class="btn btn-outline-info">Historia</button>
+                                                                  </a></td></tr>`);
+                    HTMLResponse.innerHTML = template;
+                  
+                });
+                
+                
 }
+
+getPeople();
+
+
--- Frontend_ms/js/historia_clinica.js
+++ Frontend_ms/js/historia_clinica.js
@@ -1,3 +1,116 @@
-var params = new URLSearchParams(window.location.search);
+// get Id in url
+const locationId = location.href;
+const splitURL = locationId.split('?')[1];
+const patientId = splitURL.split("=")[1];
 
-console.log(params)
+console.log(patientId)
+
+const URL = `http://localhost:8060/medicalsoft/api/clinic_history/${patientId}`;
+
+console.log(URL)
+
+const basicDataPatientSelector = document.querySelector("#datosPrincipalesPaciente");
+const dataPatientSelector = document.querySelector("#datosDelPaciente");
+
+function getClinicHistory() {
+                fetch(URL)
+                .then(response => response.json())
+                .then((patient) => {
+                    const basicDataPatient = `<div class="row col">
+                                            <div class="col-4">• N° Cedula</div>
+                                            <div class="col">${patient.patient.patientId}</div>
+                                        </div>
+                                        <div class="row col">
+                                            <div class="col-4">• Nombre</div>
+                                            <div class="col">${patient.patient.person.firstName + ' ' + patient.patient.person.lastName}</div>
+                                        </div>
+                                        <div class="row col">
+                                            <div class="col-4">• Edad</div>
+                                            <div class="col">${patient.patient.person.age}</div>
+                                        </div>
+                                        <div class="row col">
+                                            <div class="col-4">• Sexo</div>
+                                            <div class="col">${patient.patient.person.sex}</div>
+                                        </div>
+                                        <div class="row col">
+                                            <div class="col-4">• EPS</div>
+                                            <div class="col">${patient.patient.hpe.hpsName}</div>
+                                        </div>`;
+                    basicDataPatientSelector.innerHTML = basicDataPatient;
+
+
+                    const dataPatient = `<div class="row col">
+                                            <div class="col-5">• Ingreso</div>
+                                            <div class="col">${patient}</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• RH</div>
+                                            <div class="col">O+</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Ocupación</div>
+                                            <div class="col">Ingeniero de sistemas</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Nacimiento</div>
+                                            <div class="col">Sogamoso</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Residencia</div>
+                                            <div class="col">Villavicencio</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Precedencia</div>
+                                            <div class="col">Villavicencio</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Escolaridad</div>
+                                            <div class="col">Universitario</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Estado civil</div>
+                                            <div class="col">Soltero</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Religión</div>
+                                            <div class="col"> N/A</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Raza</div>
+                                            <div class="col">N/A</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Lateralidad</div>
+                                            <div class="col">Derecho</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Informante</div>
+                                            <div class="col">Prueba</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Parentesto</div>
+                                            <div class="col">Texto aquí</div>
+                                        </div>
+                                        <hr class="dropdown-divider">
+                                        <div class="row col">
+                                            <div class="col-5">• Confiabilidad</div>
+                                            <div class="col">Confiado</div>
+                                        </div>`;
+                    dataPatientSelector.innerHTML = dataPatient;
+
+                }).catch(error => console.log(error));
+}
+
+getClinicHistory();
